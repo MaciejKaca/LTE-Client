c6241d6ddd7bd048e414fea324eb106bb13b8d6c
Bartosz Wajsowicz <bartosz.wajsowicz@tieto.com>
2018-09-04 01:49:38 -0700

    Added 'common' header file.

--- client.c
+++ client.c
@@ -1,6 +1,4 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h> 
+#include "Headers/common.h"
 
 #include "Headers/preambles.h"
 #include "Headers/lte_attach.h"
@@ -16,19 +14,13 @@ int client_socket;
 
 int main(int argc, char *argv[])
 {
-	if (argc < 3) {
+	if (argc < 3)
 		error("Not enough arguments passed!");
-		exit(0);
-	}
 
 	char *server_address = argv[1];
 	int port = atoi(argv[2]);
 
 	create_session(server_address, port);
-	
-	struct RandomAccessPreamble rap;
-	rap.cyclic_prefix='z';
-	write(client_socket,(void*)&rap,sizeof(rap));
 	lte_attach();
 
 	return 0;
