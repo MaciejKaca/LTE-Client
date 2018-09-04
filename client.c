af6e47d1390078eff291eb865169e21784e25d41
Bartosz Wajsowicz <bwajsowicz@gmail.com>
2018-09-04 09:06:19 +0200

    Connection logic.

--- client.c
+++ client.c
@@ -6,13 +6,60 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-
-struct Connection
+void error(char *msg)
 {
+	perror(msg);
+	exit(0);
+}
 
-};
+int client_socket, port;
+
+void create_socket()
+{
+	client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		error("ERROR opening socket");
+}
+
+void connect_to_server(int *arguments[])
+{
+	struct sockaddr_in serv_addr;
+	struct hostent *server; 
+
+	port = arguments[2];
+
+	server = gethostbyname(arguments[1]);
+	if (server == NULL) {
+		fprintf(stderr, "ERROR, no such host!\n");
+		exit(0);
+	}
+
+	bzero((char *)&serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr,
+		(char *)&serv_addr.sin_addr.s_addr,
+		server->h_length);
+	serv_addr.sin_port = htons(portno);
+
+	if (connect(sockfd, &serv_addr, sizeof(serv_addr)) < 0)
+		error("Connection failed!");
+}
+
+void lte_attach()
+{
+	perform_random_access_procedure();
+}
 
 int main(int argc, char *argv[])
 {
+	if (argc < 3) {
+		fprintf(stderr, "Not enough arguments passed!", argv[0]);
+		exit(0);
+	}
+
+	create_socket();
+	connect_to_server(argv);
+	lte_attach();
+
 	return 0;
 }
