336686eab8a68cbb99025f88107f9bb0e0a4e20e
Bartosz Wajsowicz <bwajsowicz@gmail.com>
2018-09-04 08:11:21 +0200

    Added client source file.

---
+++ client.c
@@ -0,0 +1,18 @@
+#include "preambles.h"
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+
+struct Connection
+{
+
+};
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
