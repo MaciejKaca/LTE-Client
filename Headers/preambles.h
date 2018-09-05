71654083c2000cc0f57e23afc0fd649f84babdcf
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-09-05 02:12:09 -0700

    Fixed name from RRC_connection_request to RandomAccessResponse

--- Headers/preambles.h
+++ Headers/preambles.h
@@ -18,10 +18,10 @@ typedef struct RandomAccessPreamble
 	Sequence sequence;
 } RandomAccessPreamble;
 
-typedef struct RRC_ConnectionRequest
+typedef struct RandomAccessResponse
 {
 	Sequence sequence;
 	int timing_advance_value;
 	bool uplink_resource_grant;
 	int temp_c_rnti; // Temporary ID assigned for this session to the UE
-} RRC_ConnectionRequest;
+} RandomAccessResponse;
