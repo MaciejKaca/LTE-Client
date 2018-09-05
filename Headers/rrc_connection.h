eb2b1dcce2d8e7740cfedc8720c5bf82c2944d2a
Maciejkaca <maciek.kaca@gmail.com>
2018-09-05 13:37:28 +0200

    Finished RRC Connection structure

--- Headers/rrc_connection.h
+++ Headers/rrc_connection.h
@@ -11,3 +11,30 @@ typedef struct S_TMSI
 	int8_t MMEC; // MME Code
 	int32_t M_TMSI; // MME Mobile Subscriber Identity
 } S_TMSI;
+
+typedef struct UE_Identity
+{
+	S_TMSI s_tmsi;
+} UE_Identity
+
+typedef struct RRC_UE_ID_Type
+{
+    int c_rnti;
+} RRC_UE_ID_Type;
+
+enum EstablishmentCause // TODO: Check if this is good
+{
+	Emergency,
+	HighPriorytyAccess,
+	MobileTerminatingAccess,
+	MobileOrientedSignalling,
+	MobileOrientedData
+}
+
+typedef struct RrcConnectionRequest
+{
+    RRC_UE_ID_Type ue_id_type;
+    char ue_id_value[16]; // IMEI
+	UE_Identity ue_identity;
+	EstablishmentCause establishment_cause;
+} RrcConnectionRequest; 
