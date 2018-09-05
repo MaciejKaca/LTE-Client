1468883c6b0e7e9e8fbd1ce1f98f6ac8979c4be3
Bartosz Wajsowicz <bartosz.wajsowicz@tieto.com>
2018-09-05 02:14:36 -0700

    Added S_TMSI structure.

--- Headers/preambles.h
+++ Headers/preambles.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <stdbool.h>
+#include <stdint.h>
 
 typedef struct UE_ID_Type
 {
@@ -25,3 +26,15 @@ typedef struct RRC_ConnectionRequest
 	bool uplink_resource_grant;
 	int temp_c_rnti; // Temporary ID assigned for this session to the UE
 } RRC_ConnectionRequest;
+
+// Serving Temporary Mobile Subscriber Identity
+/*
+	First attach to network - UE sends IMSI.
+	If previously attached - UE sends S_TMSI.
+*/
+typedef struct S_TMSI 
+{
+	int8_t MMEC; // MME Code
+	int32_t M_TMSI; // MME Mobile Subscriber Identity
+} S_TMSI;
+
