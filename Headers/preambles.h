dbf91a22eed07fa7274631be656b10b10baeb051
Bartosz Wajsowicz <bartosz.wajsowicz@tieto.com>
2018-09-05 22:41:43 -0700

    Preambles  match documentation now.

--- Headers/preambles.h
+++ Headers/preambles.h
@@ -1,14 +1,10 @@
 #pragma once
 #include <stdbool.h>
-typedef struct UE_ID_Type
-{
-	int ra_rnti;
-} UE_ID_Type;
 
 typedef struct Sequence
 {
-	char ue_id_value[16]; // IMEI
-	UE_ID_Type ue_id_type;
+	char *type;
+	int ra_rnti;
 } Sequence;
 
 typedef struct RandomAccessPreamble
--- lte_attach.c
+++ lte_attach.c
@@ -7,11 +7,12 @@ RandomAccessPreamble create_input_preamble()
 {
     RandomAccessPreamble rap;
     rap.cyclic_prefix = ' ';
-    strncpy(rap.sequence.ue_id_value, "355721065083448", 16);
 
     int frequency = rand() % 64;
     int create_time = time(NULL);
-    rap.sequence.ue_id_type.ra_rnti = 1 + create_time + 10 * frequency;
+    
+    strncpy(rap.sequence.type, "RA-RNTI", 8);
+    rap.sequence.ra_rnti = 1 + create_time + 10 * frequency;
 
     return rap;
 }
@@ -19,8 +20,8 @@ RandomAccessPreamble create_input_preamble()
 bool validate_preamble(RandomAccessPreamble rap,
                        RandomAccessResponse output_preamble)
 {
-    return rap.sequence.ue_id_type.ra_rnti ==
-           output_preamble.sequence.ue_id_type.ra_rnti;
+    return rap.sequence.ra_rnti ==
+           output_preamble.sequence.ra_rnti;
 }
 
 void perform_random_access_procedure()
