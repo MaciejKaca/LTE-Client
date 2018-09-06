573127a22ea73c0ca1d30e30be7d69a5579d9136
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-09-06 02:00:02 -0700

    Writing/reading rrc connection setup

--- lte_attach.c
+++ lte_attach.c
@@ -1,7 +1,10 @@
 #include "Headers/lte_attach.h"
 #include "Headers/preambles.h"
+#include "Headers/rrc_connection.h"
 
 extern int client_socket;
+
+int C_RNTI;
 
 RandomAccessPreamble create_input_preamble()
 {
@@ -10,7 +13,7 @@ RandomAccessPreamble create_input_preamble()
 
     int frequency = rand() % 64;
     int create_time = time(NULL);
-    
+
     strncpy(rap.sequence.type, "RA-RNTI", 8);
     rap.sequence.ra_rnti = 1 + create_time + 10 * frequency;
 
@@ -20,8 +23,7 @@ RandomAccessPreamble create_input_preamble()
 bool validate_preamble(RandomAccessPreamble rap,
                        RandomAccessResponse output_preamble)
 {
-    return rap.sequence.ra_rnti ==
-           output_preamble.sequence.ra_rnti;
+    return rap.sequence.ra_rnti == output_preamble.sequence.ra_rnti;
 }
 
 void perform_random_access_procedure()
@@ -38,7 +40,63 @@ void perform_random_access_procedure()
     if (result == false)
         error("Preambles do not match.");
 
-    printf("Attach succeded. Your temp ID: %d", output_preamble.temp_c_rnti);
+    C_RNTI = output_preamble.temp_c_rnti;
+    printf("Attach succeded. Your temp ID: %d", C_RNTI);
 }
 
-void lte_attach() { perform_random_access_procedure(); }
+RRC_Connection_Request create_rrc_c_request()
+{
+    RRC_Connection_Request rrc_c_request;
+
+    strncpy(rrc_c_request.rnti_type, "C-RNTI", 8);
+    rrc_c_request.c_rnti = C_RNTI;
+
+    strncpy(rrc_c_request.ue_identity, "470010171566423", 16);
+    rrc_c_request.establishment_cause = Emergency;
+
+    return rrc_c_request;
+}
+
+RRC_Connection_Setup_Complete create_rrc_c_setup_complete()
+{
+    RRC_Connection_Setup_Complete rrc_c_setup_complete;
+
+    char plmn[7];
+    strncpy(plmn, "260", 7);
+    strcat(plmn, "06");
+
+    strncpy(rrc_c_setup_complete.plmn, plmn, 7);
+    strcpy(rrc_c_setup_complete.dedicated_info_nas, "C1332BCCAD1231BAFF21");
+
+    return rrc_c_setup_complete;
+}
+
+void rrc_connection_setup()
+{
+    RRC_Connection_Request rrc_c_request = create_rrc_c_request();
+
+    int result =
+        write(client_socket, (void *)&rrc_c_request, sizeof(rrc_c_request));
+    if (result < 0)
+        error("Couldn't write rrc_c_request to server.");
+
+    RRC_connection_Setup rrc_c_setup;
+    read(client_socket, (void *)&rrc_c_setup, sizeof(rrc_c_setup));
+
+    if (rrc_c_setup.c_rnti == C_RNTI)
+        printf("Received correct rrc_c_setup");
+
+    RRC_Connection_Setup_Complete rrc_c_setup_complete =
+        create_rrc_c_setup_complete();
+
+    int result = write(client_socket, (void *)&rrc_c_setup_complete,
+                       sizeof(rrc_c_setup_complete));
+    if (result < 0)
+        error("Couldn't write rrc_c_setup_complete to server.");
+}
+
+void lte_attach()
+{
+    perform_random_access_procedure();
+    rrc_connection_setup();
+}
