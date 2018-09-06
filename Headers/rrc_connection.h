89d669bbb6d92109ee524b4455ceb47e1c8dceae
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-09-05 23:37:48 -0700

    fixed rrc_connection_request structure

--- Headers/rrc_connection.h
+++ Headers/rrc_connection.h
@@ -1,12 +1,7 @@
 #pragma once
 #include <stdint.h>
 
-typedef struct RRC_UE_ID_Type
-{
-	int c_rnti;
-} RRC_UE_ID_Type;
-
-enum EstablishmentCause // TODO: Check if this is good
+enum EstablishmentCause
 {
 	Emergency,
 	HighPriorytyAccess,
@@ -17,8 +12,8 @@ enum EstablishmentCause // TODO: Check if this is good
 
 typedef struct RrcConnectionRequest
 {
-	RRC_UE_ID_Type ue_id_type;
-	char ue_id_value[16]; // IMEI
+	char c_rnti_type[8];
+	int c_rnti;
 	char ue_identity[16]; // IMSI
 	EstablishmentCause establishment_cause;
 } RrcConnectionRequest;
