f776e02d2030ca6f00124cc0b35659fe3e6199d3
Bartosz Wajsowicz <bartosz.wajsowicz@tieto.com>
2018-09-06 02:36:14 -0700

    Description for RCC Connection.

--- lte_attach.c
+++ lte_attach.c
@@ -24,46 +24,46 @@ bool validate_preamble(RandomAccessPreamble rap,
   return rap.sequence.ra_rnti == output_preamble.sequence.ra_rnti;
 }
 
-void perform_random_access_procedure()
-{
-    RandomAccessPreamble rap = create_input_preamble();
+void perform_random_access_procedure() {
+  printf("Started: Random Access Procedure\n");
 
-    printf("Started: Random Access Procedure\n");
-    printf("Sending: Random Access Preamble...\n");
-    printf("---\n");
-    printf("Cyclic Prefix: %d\n", rap.cyclic_prefix);
-    printf("%s: %d\n", rap.sequence.type, rap.sequence.ra_rnti);
-    printf("---\n");
-    
-    int result = write(client_socket, (void *)&rap, sizeof(rap));
-    
-    if (result < 0)
-        error("Couldn't write preamble to the server.");
-    else
-        printf("Preamble sent successfully!\n\n");
+  RandomAccessPreamble rap = create_input_preamble();
 
-    printf("Reading: Random Access Response...\n");
+  printf("Sending: Random Access Preamble...\n");
+  printf("---\n");
+  printf("Cyclic Prefix: %d\n", rap.cyclic_prefix);
+  printf("%s: %d\n", rap.sequence.type, rap.sequence.ra_rnti);
+  printf("---\n");
 
-    RandomAccessResponse output_preamble;
-    result = read(client_socket, &output_preamble, sizeof(output_preamble));
+  int result = write(client_socket, (void *)&rap, sizeof(rap));
 
-    if (result < 0)
-        error("Couldn't read output preamble from the server.");
-    else
-        printf("Preamble response red successfully!\n");
+  if (result < 0)
+    error("Couldn't write preamble to the server.");
+  else
+    printf("Preamble sent successfully!\n\n");
 
-    result = validate_preamble(rap, output_preamble);
-    if (result == false)
-        error("Preambles do not match.\n");
-    else
-        printf("%s's match!\n\n", rap.sequence.type);
+  printf("Reading: Random Access Response...\n");
 
-    printf("Random Acces Procedure succeded! \n");
-    printf("---\n");
-    printf("Timing Advance Value: %d\n", output_preamble.timing_advance_value);
-    printf("Uplink Resource Grant: %d\n", output_preamble.uplink_resource_grant);
-    printf("Your C-RNTI: %d\n", output_preamble.temp_c_rnti);
-    printf("---\n");
+  RandomAccessResponse output_preamble;
+  result = read(client_socket, &output_preamble, sizeof(output_preamble));
+
+  if (result < 0)
+    error("Couldn't read output preamble from the server.");
+  else
+    printf("Preamble response red successfully!\n");
+
+  result = validate_preamble(rap, output_preamble);
+  if (result == false)
+    error("Preambles do not match.\n");
+  else
+    printf("%s's match!\n\n", rap.sequence.type);
+
+  printf("Random Acces Procedure succeded! \n");
+  printf("---\n");
+  printf("Timing Advance Value: %d\n", output_preamble.timing_advance_value);
+  printf("Uplink Resource Grant: %d\n", output_preamble.uplink_resource_grant);
+  printf("Your C-RNTI: %d\n", output_preamble.temp_c_rnti);
+  printf("---\n");
 }
 
 RRC_Connection_Request create_rrc_c_request() {
@@ -92,18 +92,26 @@ RRC_Connection_Setup_Complete create_rrc_c_setup_complete() {
 }
 
 void rrc_connection_setup() {
+  printf("Started: RRC Connection Setup\n");
+
   RRC_Connection_Request rrc_c_request = create_rrc_c_request();
 
   int result =
       write(client_socket, (void *)&rrc_c_request, sizeof(rrc_c_request));
   if (result < 0)
     error("Couldn't write rrc_c_request to server.");
+  else
+    printf("RRC Connection request send.\n");
 
   RRC_connection_Setup rrc_c_setup;
-  read(client_socket, (void *)&rrc_c_setup, sizeof(rrc_c_setup));
+  result = read(client_socket, (void *)&rrc_c_setup, sizeof(rrc_c_setup));
+  if (result < 0)
+    error("Couldn't read rrc_c_setup from server.");
+  else
+    printf("RRC Connection Setup received.\n");
 
   if (rrc_c_setup.c_rnti == C_RNTI)
-    printf("Received correct rrc_c_setup");
+    printf("RRC Connection Setup is correct.\n");
 
   RRC_Connection_Setup_Complete rrc_c_setup_complete =
       create_rrc_c_setup_complete();
@@ -111,7 +119,11 @@ void rrc_connection_setup() {
   result = write(client_socket, (void *)&rrc_c_setup_complete,
                  sizeof(rrc_c_setup_complete));
   if (result < 0)
-    error("Couldn't write rrc_c_setup_complete to server.");
+    error("Couldn't write rrc_c_setup_complete to server.\n");
+  else
+    printf("RRC Connection Setup Complete send.\n");
+
+  printf("RRC Connection succeded.\n");
 }
 
 void lte_attach() {
