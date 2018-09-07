e93fc213e8e9c4146dceaabe24d8b866705199de
Maciejkaca <maciek.kaca@gmail.com>
2018-09-07 13:20:26 +0200

    Straing  battery thread in main.

--- client.c
+++ client.c
@@ -4,8 +4,10 @@
 #include "Headers/lte_attach.h"
 #include "Headers/user_equipment.h"
 #include "Headers/preambles.h"
+#include <pthread.h>
 
 int client_socket;
+extern UserEquipment user_equipment;
 
 int main(int argc, char *argv[])
 {
@@ -15,11 +17,13 @@ int main(int argc, char *argv[])
 
 	char *server_address = argv[1];
 	int port = atoi(argv[2]);
-	
+
 	create_user_equipment();
 	create_session(server_address, port);
-	
+
 	lte_attach();
+
+	user_equipment.baterry.baterry_drain_start();
 
 	close(client_socket);
 	return 0;
