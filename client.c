4a72f9869225d5db2e7b48536edba09feb11f2a1
Maciejkaca <maciek.kaca@gmail.com>
2018-09-11 12:46:05 +0200

    Detecting Ctrl+C

--- client.c
+++ client.c
@@ -30,6 +30,7 @@ int main(int argc, char *argv[])
 
 	thread_pool = thpool_init(2);
 
+	user_equipment.power_off_on_trigger();
 	thpool_add_work(thread_pool, (void *)start_server_listening_thread, NULL);
 	thpool_add_work(thread_pool, user_equipment.battery.battery_drain, NULL);
 
--- user_equipment.c
+++ user_equipment.c
@@ -1,7 +1,28 @@
 #include "Headers/user_equipment.h"
+#include "Headers/connection.h"
 #include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
 
 extern UserEquipment user_equipment;
+extern threadpool thread_pool;
+extern int client_socket;
+
+void INThandler(int sig)
+{
+    char shutdown[] = "UE_Shutdown";
+
+    message_label shutdown_label = {
+        message_type : msg_ue_shutdown,
+        message_length : sizeof(shutdown)
+    };
+
+    signal(sig, SIG_IGN);
+    send_data(client_socket, (void *)shutdown, sizeof(shutdown), &shutdown_label);
+    printf("Shutdown UE\n");
+    close(client_socket);
+    exit(0);
+}
 
 void create_user_equipment_imsi()
 {
@@ -16,12 +37,17 @@ void create_user_equipment_plmn()
     strcat(user_equipment.plmn, user_equipment.mnc);
 }
 
+void power_off_on_trigger()
+{
+    signal(SIGINT, INThandler);
+}
+
 UserEquipment create_user_equipment()
 {
     strncpy(user_equipment.mmc, "260", 3);
     strncpy(user_equipment.mnc, "06", 2);
     strncpy(user_equipment.msin, "123456789", 9);
-
+    user_equipment.power_off_on_trigger = power_off_on_trigger;
     create_user_equipment_plmn();
     create_user_equipment_imsi();
 
