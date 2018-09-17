d61a563c57efe8929f5ed9b128e5ce04f6bd518b
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-09-17 03:44:32 -0700

    Deleted trash comment

--- Headers/message_label.h
+++ Headers/message_label.h
@@ -23,25 +23,3 @@ typedef struct message_label
 	Message_Type message_type;
 	unsigned int message_length;
 } message_label;
-/*
-printf("---File download started!---\n");
-	for (int i = 0; i < download_info.number_of_packets; i++)
-	{
-		message_label label;
-		Download_Packet packet;
-		memset(&label, 0, sizeof(message_label));
-		memset(&packet, 0, sizeof(Download_Packet));
-
-		do
-		{
-			read(client_socket, (void *)&packet, sizeof(label));
-		} while (label.message_type != msg_download_packet);
-
-		read(client_socket, (void *)&packet, sizeof(packet));
-		if (packet.packet_number == i)
-			printf("Received packet %d/%d", i, download_info.number_of_packets);
-		FILE *file = fopen(download_info.filename, "a");
-		fprintf(file, "%s", packet.data);
-	}
-	printf("---File received!---\n");
-	*/
