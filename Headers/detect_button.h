39ba54bc014b9eafe57c01d149c5fa53840635c5
Maciejkaca <maciek.kaca@gmail.com>
2018-09-19 12:47:33 +0200

    Changed method of sending handover request
    https://trello.com/c/JmJeZHL5/28-change-method-of-sending-handover-request

--- Headers/detect_button.h
+++ Headers/detect_button.h
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "user_equipment.h"
 #include "connection.h"
-
+#include "server_handling.h"
+#include "message_label.h"
 void detect_button();
--- detect_button.c
+++ detect_button.c
@@ -1,22 +1,33 @@
 #include "Headers/detect_button.h"
-
 extern UserEquipment user_equipment;
 extern bool handover_response;
+extern int client_socket;
 
 void detect_button()
 {
-    int c;
+    int pressed_key;
+    bool handover_sended = false;
+    char handover_msg[] = "handover_request";
+    message_label handover_label = {
+        message_type : msg_handover_request,
+        message_length : sizeof(handover_msg)
+    };
     do
     {
         system("/bin/stty cbreak");
-        c = getchar();
-        switch (c)
+        pressed_key = getchar();
+        switch (pressed_key)
         {
-        case 'a':
+        case 'd':
             user_equipment.is_requesting_download = true;
             break;
         case 'h':
-            handover_response = true;
+            if(handover_sended == false)
+            {
+                send_data(client_socket, (void *)handover_msg, handover_label);
+                print_sent_data_type("msg_handover_request");
+            }
+            handover_sended = true;
             break;
         default:
             printf("------------------------\n");
