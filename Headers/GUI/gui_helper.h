108f716f4654cce12c7dc5b45bc8f6589c52ec74
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2018-09-25 05:21:07 -0700

    Display and clean screen
    Added border around screen and fixed writing strings

--- Headers/GUI/gui_helper.h
+++ Headers/GUI/gui_helper.h
@@ -2,6 +2,15 @@
 
 void place_string_on_screen(char screen[80], int x, char text[80], int size)
 {
+	bool writing_spaces = false;
+
 	for (int i = 0; i < size; i++)
-		screen[x + i] = text[i];
+	{
+		if (text[i] == '\0')
+			writing_spaces = true;
+		if (writing_spaces)
+			screen[x + i] = ' ';
+		else
+			screen[x + i] = text[i];
+	}
 }
--- gui.c
+++ gui.c
@@ -1,34 +1,41 @@
 #include "Headers/GUI/gui.h"
 #include "Headers/GUI/gui_battery.h"
 
-char screen[23][80];
-
 GUI_Battery gui_battery;
 
-void create_screen()
+void create_screen(char screen[23][80])
 {
 	for (int y = 0; y < 23; y++)
 		for (int x = 0; x < 80; x++)
+		{
 			screen[y][x] = ' ';
+			if (x == 0 || x == 79)
+				screen[y][x] = '|';
+			if (y == 0 || y == 22)
+				screen[y][x] = '-';
+		}
 }
 
-void draw_screen()
+void draw_screen(char screen[23][80])
 {
 	for (int y = 0; y < 23; y++)
+	{
 		for (int x = 0; x < 80; x++)
 			printf("%c", screen[y][x]);
-	printf("\n");
+		printf("\n");
+	}
 }
 
 void draw_gui()
 {
-	create_screen();
+	char screen[23][80];
+	create_screen(screen);
 	initialize_gui_battery();
 
 	while (true)
 	{
 		clear();
-		draw_screen();
+		draw_screen(screen);
 		gui_battery.draw_on_screen(screen);
 		sleep(1);
 	}
